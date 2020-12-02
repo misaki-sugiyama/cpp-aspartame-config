012d60d5d5f1d28a0e63556ce7fa2441446b757b
Misaki Sugiyama <misaki-sugiyama@users.noreply.github.com>
2020-12-03 00:12:18 +0800

    ?????

--- src/aspartame/registry.h
+++ src/aspartame/registry.h
@@ -4,6 +4,22 @@
 
 namespace aspartame {
 
+  class OptBase; // forward decleration
+
+  enum class OptionType { scalar, array, map };
+
+  class OptionRegistry {
+  public:
+    void add(const char* name, OptBase* opt);
+
+    bool has(const char* name);
+
+  private:
+    class Impl;
+    sugiyama::PImpl<Impl> pimpl;
+  };
+
+
   // Forward-declared Option base class
   class IOpt;
 
